43d547ea54a79ba5876445d138397306a1b4c5b6
djr2344 <djr2344@gmail.com>
2019-09-01 18:34:32 -0600

    update and new files
    -updated wordCount.py for additional text scrubbing
    -added w_cc.py   which is a frankenstein of wordCount.py and the testing algorithm from wordCountTesting.py
    -added a screenshot of the results for w_cc.py with both text files
    
    NOTE: w_cc.py was created to bypass issues using the wordCountTest.py file, I was encountering an error with the subprocess call.

--- w_cc.py
+++ w_cc.py
@@ -22,7 +22,7 @@ def ExtractAlphanumeric(InputString):
     return "".join([ch for ch in InputString if ch in (ascii_letters + digits + ' ')])
 
 # set input and output files
-if len(sys.argv) is not 3:
+if len(sys.argv) is not 4:
     print("Correct usage: <input text file> <output file>")
     for argg in sys.argv:
             print(argg)
@@ -32,6 +32,7 @@ for argg in sys.argv:
 
 textFname = sys.argv[1]
 outputFname = sys.argv[2]
+inputFname = sys.argv[3]
 
 #make sure text files exist
 if not os.path.exists(textFname):
@@ -75,4 +76,68 @@ with open(outputFname, 'w') as outputFile:
             outputFile.write('\n')
         empt = empt + 1
     
-sys.exit()
+#stats
+passed = True
+faults = 0
+words  = 0
+
+#master dictionary
+master = {}
+#dictionary to test
+test = {}
+
+# attempt to open input file
+with open(inputFname, 'r') as inputFile:
+    for line in inputFile:
+        # get rid of newline characters
+        line = line.strip()
+        # split line on whitespace and punctuation
+        word = re.split('[ \t]', line)
+        if len(word) != 2:
+            print ("Badly formatted line, exiting. Bad line:\n %s" % line)
+            exit()
+        master[word[0]] = int(word[1])
+        words += 1
+
+with open(outputFname, 'r') as outputFile:
+    lastWord = ""
+    for line in outputFile:
+        # get rid of newline characters
+        line = line.strip()
+        # split line on whitespace and punctuation
+        word = re.split('[ \t]', line)
+        if len(word) != 2:
+            print ("Badly formatted line, exiting. Bad line:\n %s" % line)
+            exit()
+        if word[0] <= lastWord:
+            print ("Misordered words: %s appears before %s" % (lastWord, word[0]))
+            passed = False
+            faults += 1
+        test[word[0]] = int(word[1])
+        lastWord = word[0]
+        
+# see if test is missing words from master
+for key in master:
+    if key not in test:
+        print ("Missing word in test file: %s" % key)
+        passed = False
+        faults += 1
+
+# see if test has words master doesn't have
+for key in test:
+    if key not in master:
+        print ("Extra word in test file: %s" % key)
+        passed = False
+        faults += 1
+        
+# see if counts match        
+for key in master:
+    if key in test and test[key] != master[key]:
+        print ("Count mismatch for %s, should be %s value is %s" % (key, master[key], test[key]))
+        passed = False
+        faults += 1
+if passed:
+    print ("Passed!")
+else:
+    print ("Error rate {0:.3f}%".format(faults * 100.0 / words))
+    print ("Failed!")
