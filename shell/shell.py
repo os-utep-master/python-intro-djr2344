3be057b206684c0252b0b6b55eb4ad1c7f25b87d
djr2344 <djr2344@gmail.com>
2019-09-08 22:00:19 -0600

    deadline commit

--- shell/shell.py
+++ shell/shell.py
@@ -2,51 +2,45 @@
 
 import os, sys, time, re
 
-def child(pid, argss):
-   # print('\nA new child %d\n' %  os.getpid())
-   # os._exit(0)  
+def child(pid, cmd, argss):  
    print (argss)
-   #nw_dir = argss[1]
    if argss[0] == "cd":           #change directory
        print("changing dir...")
        os.chdir(argss[1])
        parent()
+       sys.exit(1)
+   elif argss[0] == "ls" :
+    for dir in re.split(":", os.getcwd()): # try each directory in the path
+        program = "%s/" % (dir)
+        #os.close(1)                 # redirect child's stdout
+        #sys.stdout = open(argss, "w")
+        #os.set_inheritable(1, True)
+        os.write(1, ("dir:   %s\n" % program).encode())   
+    os.system(cmd)
+    sys.exit(1)
    else:
-    print("trying exe: \n")
-    os.execv('/bin/echo', ['foo', 'bar'])
-    os.execv('/bin/cd', ['foo', 'try_dir'])
+    # print("trying exe: \n")
+    # os.execv('/bin/echo', ['foo', 'bar'])
     
-    os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
-    args = ["wc", "p3-exec.py"]
+    # os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
+    # args = ["wc", "p3-exec.py"]
 
-    os.close(1)                 # redirect child's stdout
-    sys.stdout = open("p4-output.txt", "w")
-    os.set_inheritable(1, True)
+    # os.close(1)                 # redirect child's stdout
+    # sys.stdout = open("p4-output.txt", "w")
+    # os.set_inheritable(1, True)
 
-    for dir in re.split(":", os.environ['PATH']): # try each directory in path
-        program = "%s/%s" % (dir, args[0])
-        os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
-        try:
-            os.execve(program, args, os.environ) # try to exec program
-        except FileNotFoundError:             # ...expected
-            pass                              # ...fail quietly 
-    os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
+    # for dir in re.split(":", os.environ['PATH']): # try each directory in path
+        # program = "%s/%s" % (dir, args[0])
+        # os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
+        # try:
+            # os.execve(program, args, os.environ) # try to exec program
+        # except FileNotFoundError:             # ...expected
+            # pass                              # ...fail quietly 
+    # os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
     sys.exit(1)                 # terminate with error
 
 def parent():
    while True:
-      # newpid = os.fork()
-      # #print("forked \n")
-      # if newpid == 0:
-         # child()
-      # else:
-         # pids = (os.getpid(), newpid)
-         # print("parent: %d, child: %d\n" % pids)
-      # reply = input("q for quit / c for new fork")
-      # if reply == 'c': 
-          # continue
-      # else:
-          # break
       inputt = ""
       pid = os.getpid()               # get and remember pid
       inputt = input(os.getcwd() + "$ ")
@@ -62,7 +56,7 @@ def parent():
           sys.exit(1)
 
       elif rc == 0:                   # child
-          child(pid, argss)
+          child(pid, inputt, argss)
           
 
       else:                           # parent (forked ok)
@@ -71,8 +65,6 @@ def parent():
           childPidCode = os.wait()
           os.write(1, ("Parent: Child %d terminated with exit code %d\n" % 
                  childPidCode).encode())
-      
-      #input = input("q for quit / c for new fork \n" + os.getcwd() + "$ " )
       if inputt == "exit": 
           print("Exiting shell...")
           sys.exit(1)
