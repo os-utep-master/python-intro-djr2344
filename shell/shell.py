6707839e8ab3eaa5711b746a7e7ad596f3376221
djr2344 <djr2344@gmail.com>
2019-09-22 18:56:17 -0600

    update
    -cleaned out unnecessary prints
    -added saftey nets for functionality
    -pipes still not functioning

--- shell/shell.py
+++ shell/shell.py
@@ -3,10 +3,17 @@
 import os, sys, time, re
 
 def child(pid, cmd, argss):  
-   print ("command is: " + str(argss))
-   os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
+   #print ("command is: " + str(argss))
+   #os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
    #deal with pipes --------------------------------------------------
-   if "|" in argss:           
+   if "<" in argss:
+    os.system(cmd) #functionality not yet implemented for redirect input
+   elif "sleep" in argss:
+    os.system(cmd) #functionality not yet implemented for background process
+   elif "|" in argss: 
+    #run with os.system() for functionality
+    os.system(cmd)
+    #attempt to run with pipe & fork call
     stp = argss.index("|")
     #print (stp)
     frst_args = [] # the command before the first pipe
@@ -19,21 +26,21 @@ def child(pid, cmd, argss):
         elif i > stp:
                 scnd_args.append(a)   #grab the command after "|"
         i = i + 1
-    print ("first command to be executed: " + str(frst_args))
+    #print ("first command to be executed: " + str(frst_args))
     fcmd = ""
     for c in frst_args:
         fcmd = fcmd + c
-    print ("second command to be executed: " + str(scnd_args))
+    #print ("second command to be executed: " + str(scnd_args))
     pid = os.getpid()               # get and remember pid
     
     #pipe stuff
     pr,pw = os.pipe()
     for f in (pr, pw):
         os.set_inheritable(f, True)
-    print("pipe fds: pr=%d, pw=%d" % (pr, pw))
+    #print("pipe fds: pr=%d, pw=%d" % (pr, pw))
     import fileinput
     
-    os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
+    #os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
 
     rc = os.fork()
 
@@ -42,7 +49,7 @@ def child(pid, cmd, argss):
           sys.exit(1)
 
     elif rc == 0:                   # child
-          print("Child: My pid==%d.  Parent's pid=%d" % (os.getpid(), pid), file=sys.stderr)
+          #print("Child: My pid==%d.  Parent's pid=%d" % (os.getpid(), pid), file=sys.stderr)
           os.close(1)                 # redirect child's stdout
           os.dup(pw)
           
@@ -51,25 +58,25 @@ def child(pid, cmd, argss):
           child(pid, fcmd, frst_args)
           
     else:                           # parent (forked ok)
-          print("Parent: My pid==%d.  Child's pid=%d" % (os.getpid(), rc), file=sys.stderr)
+          #print("Parent: My pid==%d.  Child's pid=%d" % (os.getpid(), rc), file=sys.stderr)
           os.close(0)
           os.dup(pr)
           for fd in (pw, pr):
               os.close(fd)
           for line in fileinput.input():
-              print("From child: <%s> \n" % line)
+              print("From piped child: <%s> \n" % line)
 
 #----------------------------------------------------------------------------------------------------   
    elif argss[0] == "cd":           #change directory
-       print("changing dir...")
+       #print("changing dir...")
        os.chdir(argss[1])
        parent()
        sys.exit(1)
-
+#----------------------------------------------------------------------------------------------------
    elif argss[0] == "ls":    #list directory
-     print("trying ls : \n")
+     #print("trying ls : \n")
      if ">" in argss :        #redirect output
-        print("redirecting output... \n")
+        #print("redirecting output... \n")
         sp = argss.index(">")
         nw_args = []
         i = 0
@@ -80,14 +87,14 @@ def child(pid, cmd, argss):
             elif argss.index(a) > sp:
                 outp = a   #grab where the output is to be redirected
             i = i + 1
-        print ("command to be executed: " + str(nw_args))
-        print ("redirected output to: " + outp)
+        #print ("command to be executed: " + str(nw_args))
+        #print ("redirected output to: " + outp)
         os.close(1)
         sys.stdout = open(outp, "w")
         os.set_inheritable(1, True)
         for dir in re.split(":", os.environ['PATH']): # try each directory in path
          program = "%s/%s" % (dir, nw_args[0])
-         os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
+         #os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
          try:
              #print (os.environ)
              os.execve(program, nw_args, os.environ) # try to exec program
@@ -99,7 +106,7 @@ def child(pid, cmd, argss):
      else:
         for dir in re.split(":", os.environ['PATH']): # try each directory in path
             program = "%s/%s" % (dir, argss[0])
-            os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
+            #os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
             try:
                 #print (os.environ)
                 os.execve(program, argss, os.environ) # try to exec program
@@ -117,9 +124,7 @@ def parent():
       pid = os.getpid()               # get and remember pid
       inputt = input(os.getcwd() + "$ ")
       argss = re.split('\s', inputt)  #get cmd args
-      #for a in argss:
-      #    print (a)
-      os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
+      #os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
 
       rc = os.fork()
 
@@ -132,7 +137,7 @@ def parent():
           
 
       else:                           # parent (forked ok)
-          os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % (pid, rc)).encode())
+          #os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % (pid, rc)).encode())
           
           childPidCode = os.wait()
           os.write(1, ("Parent: Child %d terminated with exit code %d\n" % 
